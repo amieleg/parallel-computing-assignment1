3d8f3a84cc1f4ba40f60a381db872137dafbcf55
Ivo Melse <ivo@melse.info>
2023-04-29 10:44:31 +0200

    Update seq_stencil.sh

--- seq_stencil.sh
+++ seq_stencil.sh
@@ -4,11 +4,12 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=csmpi_fpga_short
 #SBATCH --time=00:05:00
-#SBATCH --output=stencil_omp.out
+#SBATCH --output=stencil_seq.out
 
 # Compile on the machine, not the head node
 make bin/stencil_seq
-
-for P in 1 2 3 4 5 6; do
-    OMP_NUM_THREADS="$P" bin/stencil_seq 10000 100 0 > results/stencil_seq_"$P".txt
+for N in 1 2 3 4 5 6; do
+    for iters in 1 2 3 4 5 6; do
+        OMP_NUM_THREADS="1" bin/stencil_seq 10000 100 0 > results/stencil_seq_"$P".txt
+    done
 done
