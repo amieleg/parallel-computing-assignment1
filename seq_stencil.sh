877665031422470f5d3ec5feabbadb3fd19ea6f0
Ivo Melse <ivo@melse.info>
2023-04-29 10:30:51 +0200

    Update seq_stencil.sh

--- seq_stencil.sh
+++ seq_stencil.sh
@@ -7,8 +7,8 @@
 #SBATCH --output=stencil_omp.out
 
 # Compile on the machine, not the head node
-make bin/stencil_omp
+make bin/stencil_seq
 
 for P in 1 2 3 4 5 6; do
-    OMP_NUM_THREADS="$P" bin/stencil_omp 10000 100 0 > results/stencil_omp_"$P".txt
+    OMP_NUM_THREADS="$P" bin/stencil_seq 10000 100 0 > results/stencil_seq_"$P".txt
 done
