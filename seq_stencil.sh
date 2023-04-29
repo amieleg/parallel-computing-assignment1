cbc2d1f0f1d01a4d3d8379d734750b12ef2a4107
Ivo Melse <ivo@melse.info>
2023-04-29 11:01:57 +0200

    Update seq_stencil.sh

--- seq_stencil.sh
+++ seq_stencil.sh
@@ -11,7 +11,7 @@ make bin/stencil_seq
 # 5 kB, 5 MB, 5 GB
 # We have to divide by 8 because a double takes up 8 bytes
 for N in 625 625000 625000000; do
-    for iters in 25 50 75 100; do
+    for iters in 500 1000 1500 2000; do
         OMP_NUM_THREADS="1" bin/stencil_seq 10000 100 0 > results/stencil_seq_"$N,$iters".txt
     done
 done
