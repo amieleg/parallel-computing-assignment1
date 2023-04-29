7b39e64bb43e24caa512c455c25fc07cffafcc6f
Ivo Melse <ivo@melse.info>
2023-04-29 10:47:26 +0200

    Update seq_stencil.sh

--- seq_stencil.sh
+++ seq_stencil.sh
@@ -8,8 +8,8 @@
 
 # Compile on the machine, not the head node
 make bin/stencil_seq
-for N in 1 2 3 4 5 6; do
+for N in 10 20 30 40 50 60; do
     for iters in 1 2 3 4 5 6; do
-        OMP_NUM_THREADS="1" bin/stencil_seq 10000 100 0 > results/stencil_seq_"$P".txt
+        OMP_NUM_THREADS="1" bin/stencil_seq 10000 100 0 > results/stencil_seq_"$N,$iters".txt
     done
 done
