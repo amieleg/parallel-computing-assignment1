c164f757e5bfb5d521fa14ea50fe9e21252c7a21
Ivo Melse <ivo@melse.info>
2023-05-11 16:25:22 +0200

    Update mpi_hello.sh

--- mpi_hello.sh
+++ mpi_hello.sh
@@ -3,7 +3,7 @@
 NODES=1
 while [ $NODES -le 8 ]
 do
-    NTASKS=$(( 8 * $NODES ))
+    NTASKS=$(( 8 * NODES ))
     
     sed "s/NODES/$NODES/g" mpi_hello.sh.template > mpi_hello_"$NTASKS".sh
     sed -i "s/NTASKS/$NTASKS/g" mpi_hello_"$NTASKS".sh
