3bad33f013eaf222e821296048b526f7414539b2
Ivo Melse <ivo@melse.info>
2023-05-25 16:14:39 +0200

    f

---
+++ run_mpi.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+NODES=1
+while [ $NODES -le 8 ]
+do
+    NTASKS=$(( 8 * NODES ))
+    
+    sed "s/NODES/$NODES/g" run_mpi.sh.template > run_mpi_"$NTASKS".sh
+    sed -i "s/NTASKS/$NTASKS/g" run_mpi_"$NTASKS".sh
+    # ./ -> sbatch
+    chmod u+x,g+wx * # Remove this on the remote
+    ./run_mpi_"$NTASKS".sh
+    
+    rm run_mpi_"$NTASKS".sh
+
+    NODES=$(( 2 * NODES ))
+done
