6529c41d0de8e77b804cc075671b99582b160c62
amiel <chickencoreore@gmail.com>
2023-05-26 09:55:24 +0200

    sruh

--- run_mpi.sh
+++ run_mpi.sh
@@ -3,15 +3,19 @@
 NODES=1
 while [ $NODES -le 8 ]
 do
-    NTASKS=$(( 8 * NODES ))
+    TASKSPERNODE=1
+    while [ $TASKSPERNODE -le 8]
+    do
+        NTASKS=$((TASKSPERNODE * NODES ))
     
-    sed "s/NODES/$NODES/g" run_mpi.sh.template > run_mpi_"$NTASKS".sh
-    sed -i "s/NTASKS/$NTASKS/g" run_mpi_"$NTASKS".sh
-    # ./ -> sbatch or sbatch -> ./
-    # chmod u+x,g+wx * # Remove this on the remote
-    sbatch run_mpi_"$NTASKS".sh
+        sed "s/NODES/$NODES/g" run_mpi.sh.template > run_mpi_"$NTASKS".sh
+        sed -i "s/NTASKS/$NTASKS/g" run_mpi_"$NTASKS".sh
+        # ./ -> sbatch or sbatch -> ./
+        # chmod u+x,g+wx * # Remove this on the remote
+        sbatch run_mpi_"$NTASKS".sh
     
-    rm run_mpi_"$NTASKS".sh
-
+        rm run_mpi_"$NTASKS".sh
+        TASKSPERNODE=$(( 2 * TASKSPERNODE ))
+    done
     NODES=$(( 2 * NODES ))
 done
